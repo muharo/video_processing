ab09b226c04704a111ac3af142264f29390526b5
Robert Muller <muller.roby@gmail.com>
2019-04-24 00:58:09 +0300

    Improve tasks publishing to queue

---
+++ video_scheduler/utils/publish_tasks.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+re='^[0-9]+$'
+
+if [[ -z $1 ]]; then
+    echo "How many messages?"
+    read counter
+else
+    counter=$1
+fi
+
+if [[ -z $counter ]]; then
+    echo "No argument provided! A number is required!"
+    exit 1
+elif ! [[ $counter =~ $re ]] ; then
+    echo "Not a valid number! A number is required!"
+    exit 1
+fi
+
+i=$counter
+for file in $( ls -1v /video/input/ ); do
+    if [[ $i -gt 0 ]]; then
+    	#echo "/usr/bin/amqp-publish --url=$BROKER_URL -r $QUEUE_NAME -p -b $file"
+        /usr/bin/amqp-publish --url=$BROKER_URL -r $QUEUE_NAME -p -b $file
+        ((i--))
+    else
+        break
+    fi
+done
+
+echo "$counter tasks added to queue"
--- video_scheduler/utils/rename_videos.sh
+++ video_scheduler/utils/rename_videos.sh
@@ -2,13 +2,13 @@
 
 # Check path argument is valid
 if [[ -z "$1" || $1 != /* ]]; then
-        echo "Invalid path! It should be an absolute path!"
-        exit 1
+    echo "Invalid path! It should be an absolute path!"
+    exit 1
 elif [[ ! -d $1 ]]; then
-        echo "Invalid path! Directory doesn't exist!"
-        exit 1
+    echo "Invalid path! Directory doesn't exist!"
+    exit 1
 else
-        path=$1
+    path=$1
 fi
 
 # Initialize counter
@@ -16,9 +16,9 @@ i=1
 
 # Do the renaming
 for file in $path/*; do
-        ext=$(echo $file | rev | cut -d'.' -f 1 | rev)
-        mv "$file" "${path}/video${i}.${ext}"
-        let "i=i+1"
+    ext=$(echo $file | rev | cut -d'.' -f 1 | rev)
+    mv "$file" "${path}/video${i}.${ext}"
+    ((i++))
 done
 
 # Print final list
