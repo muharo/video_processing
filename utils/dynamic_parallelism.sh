aab464a9f84018ed47910e1e1c35722a3099a5c4
muller89 <muller.roby@gmail.com>
2019-05-19 00:04:01 +0300

    Update dynamic parallelism script

--- utils/dynamic_parallelism.sh
+++ utils/dynamic_parallelism.sh
@@ -3,26 +3,41 @@
 read -p "Number of tasks: " tasks
 read -p "Percentage of high consuming tasks: " percentage
 
-counter=$((tasks*percentage/100))
-parallelism=9
-# echo "$counter tasks to be run as ${parallelism} parallel tasks"
-cat video_worker.template | envsubst > video_worker_1.yml
+export counter=$((tasks*percentage/100))
+export parallelism=9
+cat video_worker.template | envsubst > video_worker_1.yaml
 
+start=`date +%s`
 kubectl create -f video_worker_1.yaml
 
-# WAIT
+# wait for certain number of tasks to complete
+# before starting the second job
 while :
 do
+	sleep 1
 	count=$(kubectl get pods | grep Completed | wc -l)
 	if [ $count -eq $((counter-parallelism)) ]; then
 		break
 	fi
-	sleep 1
 done
 
-counter=$((tasks-counter))
-parallelism=15
-# echo "$counter tasks to be run as ${parallelism} parallel tasks"
-cat video_worker.template | envsubst > video_worker_2.yml
+export counter=$((tasks-counter))
+export parallelism=15
+cat video_worker.template | envsubst > video_worker_2.yaml
 
 kubectl create -f video_worker_2.yaml
+
+# wait for all jobs to complete
+while :
+do
+	sleep 1
+	count=$(kubectl get pods | grep Completed | wc -l)
+	if [ $count -eq $counter ]; then
+		break
+	fi
+done
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo "Jobs completed in $runtime seconds."
