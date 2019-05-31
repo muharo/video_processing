5bd3bc87e95eeeaa6cb1030a0aff761c87f07843
muller89 <muller.roby@gmail.com>
2019-05-31 22:45:18 +0300

    Add new script to sort by video format

---
+++ video_scheduler/utils/publish_tasks_by_type.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+path="/video/input"
+re='^[0-9]+$'
+
+# Read tasks number
+if [[ -z $1 ]]; then
+    echo ""
+    echo "How many tasks to be queued?"
+    read counter
+else
+    counter=$1
+fi
+
+if [[ -z $counter ]]; then
+    echo "No argument provided! A number is required!"
+    echo ""
+    exit 1
+elif ! [[ $counter =~ $re ]] ; then
+    echo "Not a valid number! A number is required!"
+    echo ""
+    exit 1
+elif [[ $counter -eq 0 ]]; then
+    counter=$(ls $path | wc -l)
+fi
+
+publish_tasks(){
+    i=$counter
+    for file in $list; do
+        if [[ $i -gt 0 ]]; then
+            /usr/bin/amqp-publish --url=$BROKER_URL -r $QUEUE_NAME -p -b $file
+            ((i--))
+        else
+            break
+        fi
+    done
+}
+
+if [[ -z $2 ]]; then
+
+    title="Select tasks arrangement algorithm."
+    prompt="Pick an option:"
+    options=("FIFO" "SFF" "LFF")
+
+    echo ""
+    echo "$title"
+    echo ""
+
+    PS3="$prompt "
+    select opt in "${options[@]}" "Cancel"; do
+
+        case "$REPLY" in
+
+        1)
+            algorithm="FIFO"
+            break
+            ;;
+        2)
+            algorithm="SFF"
+            break
+            ;;
+        3)
+            algorithm="LFF"
+            break
+            ;;
+        4)
+            echo ""
+            echo "Goodbye!"
+            echo ""
+            exit 0
+            ;;
+        *)
+            echo ""
+            echo "Invalid option. Try again!"
+            continue
+            ;;
+        esac
+    done
+
+else
+    algorithm=$2
+fi
+
+case "$algorithm" in
+
+    FIFO)
+        echo ""
+        echo "Ordering videos alphabetically..."
+        list=$(ls -1v *.webm $path)
+        publish_tasks
+        list=$(ls -1v *.mp4 $path)
+        publish_tasks
+        echo "$counter tasks were added to queue."
+        echo ""
+        ;;
+    SFF)
+        echo ""
+        echo "Ordering videos by size (ascending)..."
+        list=$(ls -1rS *.webm $path)
+        publish_tasks
+        list=$(ls -1rS *.mp4 $path)
+        publish_tasks
+        echo "$counter tasks were added to queue."
+        echo ""
+        ;;
+    LFF)
+        echo ""
+        echo "Ordering videos by size (descending)..."
+        list=$(ls -1S *.webm $path)
+        publish_tasks
+        list=$(ls -1S *.mp4 $path)
+        publish_tasks
+        echo "$counter tasks were added to queue."
+        echo ""
+        ;;
+    *)
+        echo ""
+        echo "Invalid algorithm. Exit!"
+        exit 1
+        ;;
+esac
